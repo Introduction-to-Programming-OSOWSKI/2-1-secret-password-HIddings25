9e87e2ce3377346be622ebd4975d031ee9a6d121
HIddings25 <829122@mystma.org>
2023-03-30 15:01:02 -0500

    2

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def password(x):
     enteredPassword = input("what is the password")
 
     if enteredPassword == "Knights19":
-       print("access granted")
+       print("access granted") 
+
+    else: 
+        print("access denied")
 
 
